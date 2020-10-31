6fba7c95a2c5a20199346847a9a8e7a7b4849f72
Daniyal Marofi <daniyaleight8@gmail.com>
2020-10-31 23:18:36 +0330

    redesgined classes and added a few methods

--- main.cpp
+++ main.cpp
@@ -6,32 +6,60 @@ class Stage;
 class Car;
 class Worker;
 
-class Stage{
+class Stage {
+   public:
+    Stage(vector<int> workers_time_coefficient) {
+        for (auto worker : workers_time_coefficient)
+            workers.push_back(Worker(worker));
+    }
+
+   private:
     vector<Worker> workers;
+    vector<Car> waiting_queue;
 };
 
-class Carwah{
-
-    vector<Car> queue;
+class Carwah {
+   public:
+   void add_stage(Stage stage){
+       stages.push_back(stage);
+   }
+   private:
+    vector<Stage> stages;
 };
 
-class Worker{
-    private:
+class Worker {
+   public:
+    Worker(int coefficient) { time_coefficient = coefficient; }
+
+   private:
     int id;
-    int sloth;
-
+    int time_coefficient;
 };
 
-class Car{
-    private:
+class Car {
+   private:
     int id;
-    int difficulty;
-
+    int luxury_coefficient;
 };
 
-int main()
-{
-    cout << "Hello Wolrd!" << endl;
+void handle_user_commands(Carwah& carwash) {
+    string command;
+    while (cin >> command) {
+        if (!command.compare("add_stage")) {
+            int n;
+            for (int i = 0; i < n; i++) {
+                int worker_time_coefficient;
+                cin >> worker_time_coefficient;
+                // carwash->add_stage();
+                // Worker(worker_time_coefficient);
+            }
+        }
+    }
+}
+
+int main() {
+    Carwah carwah;
+    handle_user_commands(carwash);
 
     return 0;
 }
