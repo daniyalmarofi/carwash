3eeb8b86631c96fbc401d5cf679cdcb43d194286
Daniyal Marofi <daniyaleight8@gmail.com>
2020-10-31 23:44:13 +0330

    implemented add worker

--- main.cpp
+++ main.cpp
@@ -8,28 +8,34 @@ class Worker;
 
 class Stage {
    public:
-    Stage(vector<int> workers_time_coefficient) {
-        for (auto worker : workers_time_coefficient)
-            workers.push_back(Worker(worker));
-    }
+    void add_worker(Worker* worker) { workers.push_back(worker); }
 
    private:
-    vector<Worker> workers;
+    vector<Worker*> workers;
     vector<Car> waiting_queue;
 };
 
-class Carwah {
+
+class Carwash {
    public:
-   void add_stage(Stage stage){
-       stages.push_back(stage);
-   }
+    void add_stage(Stage stage) { stages.push_back(stage); }
+    Worker* add_worker(int worker_time_coefficient) {
+        Worker worker=Worker(workers.size(), worker_time_coefficient);
+        workers.push_back(worker);
+        return &worker;
+    }
+
    private:
     vector<Stage> stages;
+    vector<Worker> workers;
 };
 
 class Worker {
    public:
-    Worker(int coefficient) { time_coefficient = coefficient; }
+    Worker(int worker_id, int worker_time_coefficient) {
+        id = worker_id;
+        time_coefficient = worker_time_coefficient;
+    }
 
    private:
     int id;
@@ -40,25 +46,27 @@ class Car {
    private:
     int id;
     int luxury_coefficient;
+    int timeleft;
 };
 
-void handle_user_commands(Carwah& carwash) {
+void handle_user_commands(Carwash& carwash) {
     string command;
     while (cin >> command) {
         if (!command.compare("add_stage")) {
             int n;
+            Stage stage;
             for (int i = 0; i < n; i++) {
                 int worker_time_coefficient;
                 cin >> worker_time_coefficient;
-                // carwash->add_stage();
-                // Worker(worker_time_coefficient);
+                stage.add_worker(carwash.add_worker(worker_time_coefficient));
             }
+            carwash.add_stage(stage);
         }
     }
 }
 
 int main() {
-    Carwah carwah;
+    Carwash carwash;
     handle_user_commands(carwash);
 
     return 0;
