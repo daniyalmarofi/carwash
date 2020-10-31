08bdcd4af83a4832b0ce1dee0a9511340a0ce203
Daniyal Marofi <daniyaleight8@gmail.com>
2020-10-31 22:12:32 +0330

    defined classes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+class Stage;
+class Car;
+class Worker;
+
+class Stage{
+    vector<Worker> workers;
+};
+
+class Carwah{
+
+    vector<Car> queue;
+};
+
+class Worker{
+    private:
+    int id;
+    int sloth;
+
+};
+
+class Car{
+    private:
+    int id;
+    int difficulty;
+
+};
 
 int main()
 {
