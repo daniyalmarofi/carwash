bdd08585aa48a4902fc5a80c4c7e9b7b4ae803e9
Daniyal Marofi <daniyaleight8@gmail.com>
2020-11-01 08:32:09 +0330

    tested add stage method

--- main.cpp
+++ main.cpp
@@ -2,9 +2,30 @@
 #include <vector>
 using namespace std;
 
-class Stage;
-class Car;
-class Worker;
+class Car {
+   private:
+    int id;
+    int luxury_coefficient;
+    int timeleft;
+};
+
+class Worker {
+   public:
+    Worker(int worker_id, int worker_time_coefficient) {
+        id = worker_id;
+        time_coefficient = worker_time_coefficient;
+    }
+    int get_id(){
+        return id;
+    }
+    int get_time_coefficient(){
+        return time_coefficient;
+    }
+
+   private:
+    int id;
+    int time_coefficient;
+};
 
 class Stage {
    public:
@@ -15,14 +36,16 @@ class Stage {
     vector<Car> waiting_queue;
 };
 
-
 class Carwash {
    public:
     void add_stage(Stage stage) { stages.push_back(stage); }
     Worker* add_worker(int worker_time_coefficient) {
-        Worker worker=Worker(workers.size(), worker_time_coefficient);
+        Worker worker(workers.size(), worker_time_coefficient);
         workers.push_back(worker);
         return &worker;
+    }
+    vector<Worker> get_workers(){
+        return workers;
     }
 
    private:
@@ -30,37 +53,20 @@ class Carwash {
     vector<Worker> workers;
 };
 
-class Worker {
-   public:
-    Worker(int worker_id, int worker_time_coefficient) {
-        id = worker_id;
-        time_coefficient = worker_time_coefficient;
-    }
-
-   private:
-    int id;
-    int time_coefficient;
-};
-
-class Car {
-   private:
-    int id;
-    int luxury_coefficient;
-    int timeleft;
-};
-
 void handle_user_commands(Carwash& carwash) {
     string command;
     while (cin >> command) {
         if (!command.compare("add_stage")) {
-            int n;
+            int number_of_workers;
+            cin >> number_of_workers;
             Stage stage;
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i < number_of_workers; i++) {
                 int worker_time_coefficient;
                 cin >> worker_time_coefficient;
                 stage.add_worker(carwash.add_worker(worker_time_coefficient));
             }
             carwash.add_stage(stage);
+            cout << "OK" << endl;
         }
     }
 }
@@ -68,6 +74,8 @@ void handle_user_commands(Carwash& carwash) {
 int main() {
     Carwash carwash;
     handle_user_commands(carwash);
+    // for(Worker w:carwash.get_workers())
+    //     cout<<w.get_id()<<" "<<w.get_time_coefficient()<<endl;
 
     return 0;
 }
