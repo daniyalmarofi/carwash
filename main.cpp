c3915e780755e60d4526b06ad5e240f5d651f042
Daniyal Marofi <daniyaleight8@gmail.com>
2020-11-01 10:02:04 +0330

    handled workers id

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ class Worker {
 class Stage {
    public:
     void add_worker(int worker_id, int worker_time_coefficient) {
-        Worker worker(workers.size(), worker_time_coefficient);
+        Worker worker(worker_id, worker_time_coefficient);
         workers.push_back(worker);
     }
     vector<Worker> get_workers() {
@@ -59,10 +59,10 @@ void handle_user_commands(Carwash& carwash) {
     string command;
     while (cin >> command) {
         if (!command.compare("add_stage")) {
-            int number_of_workers;
-            cin >> number_of_workers;
+            int number_of_stage_workers;
+            cin >> number_of_stage_workers;
             Stage stage;
-            for (int i = 0; i < number_of_workers; i++) {
+            for (int i = 0; i < number_of_stage_workers; i++) {
                 int worker_time_coefficient;
                 cin >> worker_time_coefficient;
                 stage.add_worker(carwash.get_new_worker_id(), worker_time_coefficient);
@@ -78,12 +78,11 @@ int main() {
     handle_user_commands(carwash);
     for (Stage s : carwash.get_stages()) {
         vector<Worker> workers = s.get_workers();
-        cout << "count: " << workers.size() << endl;
+        cout <<"stage\t" << "count: " << workers.size() << endl;
         for (Worker w : workers) {
             cout << w.get_id() << " " << w.get_time_coefficient() << endl;
         }
     }
-    // for(Worker* w:s.get_workers())
 
     return 0;
 }
